eeb6e88afaddb7d4cbfe27833e259304e1e98f7e
Dru Kelly <drukelly@gmail.com>
2019-05-17 23:08:09 -0700

    Project set up

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+// Use https://randomuser.me/ for random profile pictures?
+const friends = [{
+  "name":"John",
+  "photo":"https://randomuser.me/api/portraits/men/88.jpg",
+  "scores":[5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+}, {
+  "name":"Jane",
+  "photo":"https://randomuser.me/api/portraits/women/88.jpg",
+  "scores":[5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+}]
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,2 @@
+// TODO
+// htmlRoutes.js
