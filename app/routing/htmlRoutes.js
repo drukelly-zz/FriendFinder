741611f135f07e262cb15473ead8738791b3eb25
Dru Kelly <drukelly@gmail.com>
2019-05-31 22:28:53 -0700

    Modify htmlRoutes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,10 +5,10 @@ const path = require('path')
 
 module.exports = (app) => {
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/../public/home.html'))
+    res.sendFile(path.join(__dirname, '../public/home.html'))
   })
   
   app.get('/survey', (req, res) => {
-    res.sendFile(path.join(__dirname, '/../public/survey.html'))
+    res.sendFile(path.join(__dirname, '../public/survey.html'))
   })
 }
